81d2a8e5e50dd0a766b1a0912f2acf9477480e61
wjinss <aastu4343@gmail.com>
2025-08-12 14:26:34 +0900

    learn. 문 vs. 식 - 문은 값이 없다. - 식은 값이 있다. - 문은 함수 몸체에서만 사용 가능 - 식은 함수 몸체 또는 React.createElement() 내부에서 모두 사용 가능

--- 03-react-component/src/app.js
+++ 03-react-component/src/app.js
@@ -3,18 +3,28 @@ import { ShortcutFunction as Shortcut } from "./components/shortCut.js";
 import Output from "./components/output.js";
 
 export default function App(props) {
-  let isAnimate = true;
+  // 문 === 값이 없다 === return에서 사용 불가
+  // if (props.count >= props.targetCount) {
+  //   console.log("애니메이션 종료!");
+  //   isAnimate = false;
+  // }
 
-  if (props.count >= props.targetCount) {
-    console.log("애니메이션 종료!");
-    isAnimate = false;
-  }
+  // 식 === 값이 있다 === return에서 사용 가능
+  // const isComplete = props.count >= props.targetCount;
+  // console.log(isComplete);
+  // 논리 연산자 식
+  // 삼항 연산자 식
+  // 옵셔널 체이닝, null 병합 연산자
 
   return React.createElement(
     "div",
     { className: "randomCountUpApp" },
     React.createElement(Logo),
-    React.createElement(Output, { isAnimate }, props.count),
+    React.createElement(
+      Output,
+      { isAnimate: props.count < props.targetCount ? true : false },
+      props.count
+    ),
     React.createElement(Shortcut)
   );
 }
