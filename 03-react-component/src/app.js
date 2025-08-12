f7ca6e96833f90b023ddaf5161e3ac6c9ebe7461
wjinss <aastu4343@gmail.com>
2025-08-12 15:08:08 +0900

    learn. 리스트 렌더링 - 배열 데이터를 사용해 리스트 렌더링 - 반복문은 함수 몸체에서만 사용 가능 (값이 없음) - 배열의 map 메서드, filter 메서드와 같이 새로운 배열을 반환하는 메서드 (값이 있음)

--- 03-react-component/src/app.js
+++ 03-react-component/src/app.js
@@ -1,30 +1,59 @@
 import { LogoFunction as Logo } from "./components/logo.js";
-import { ShortcutFunction as Shortcut } from "./components/shortCut.js";
+import { ShortcutFunction as Shortcut } from "./components/shortcut.js";
 import Output from "./components/output.js";
 
 export default function App(props) {
-  // 문 === 값이 없다 === return에서 사용 불가
+  // 문 (값이 없다)
   // if (props.count >= props.targetCount) {
-  //   console.log("애니메이션 종료!");
-  //   isAnimate = false;
+  //   isAnimate = false
   // }
 
-  // 식 === 값이 있다 === return에서 사용 가능
-  // const isComplete = props.count >= props.targetCount;
-  // console.log(isComplete);
+  // createElement() 함수 내부에서는 문을 사용할 수 없다.
+  // 왜? 값이 없으니까! 문 대신, 식을 사용해야 한다.
+
+  // 식 (값이 있다)
+  // const isComplete = props.count >= props.targetCount
   // 논리 연산자 식
   // 삼항 연산자 식
-  // 옵셔널 체이닝, null 병합 연산자
+  // 옵셔널 체이닝(?.), null 병합 연산자(??)
+
+  // 리스트 렌더링(List Rendering, Render Lists)
+  // 배열(데이터)
+
+  // 배열 순환문 (for, while, for...of 등) -> 값이 없음 (함수 몸체 안에서만)
+  // let renderElements = []
+
+  // for (let i = 0; i < 3; i++) {
+  //   renderElements.push(React.createElement(
+  //     Output,
+  //     {
+  //       key: i,
+  //       isAnimate: (props.count < props.targetCount)
+  //         ? true
+  //         : false
+  //     },
+  //     props.count + i
+  //   ))
+  // }
+
+  // 배열의 forEach 메서드 (값이 없음) ❌
+  // 배열의 map 메서드 (값이 있음) ✅
+  const renderElements = Array.from({ length: 3 }).map((_, index) =>
+    React.createElement(
+      Output,
+      {
+        key: index,
+        isAnimate: props.count < props.targetCount ? true : false,
+      },
+      props.count + index
+    )
+  );
 
   return React.createElement(
     "div",
     { className: "randomCountUpApp" },
     React.createElement(Logo),
-    React.createElement(
-      Output,
-      { isAnimate: props.count < props.targetCount ? true : false },
-      props.count
-    ),
+    renderElements,
     React.createElement(Shortcut)
   );
 }
--- 03-react-component/src/components/shortCut.js
+++ 03-react-component/src/components/shortCut.js
@@ -5,6 +5,14 @@ export function ShortcutFunction() {
     message: " 키를 누르면 애니메이션이 다시 시작됩니다.",
   };
 
+  // 리액트 + jsx
+  // return (
+  //   <p className={data.className}>
+  //     <code>{data.shortcutKey}</code>
+  //     {data.message}
+  //   </p>
+  // );
+
   // 리액트 엘리먼트 생성 과정에서 props로 데이터 바인딩(data binding)
   return React.createElement(
     "p",
