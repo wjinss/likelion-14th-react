53e6101d89bd72c6e275e128d31f0a03e972fb16
wjinss <aastu4343@gmail.com>
2025-08-12 11:45:01 +0900

    learn. 클래스 컴포넌트 - class 구문 (extends React.Component) - 컴포넌트 이름 -> 파스칼케이스로 작명 - 컴포넌트 -> 엘리먼트 생성 - 컴포넌트 속성(props) ->  다양한 엘리먼트 생성 - 컴포넌트 트리 구성 (this.props.children)

--- 03-react-component/src/components/class.js
+++ 03-react-component/src/components/class.js
@@ -1,2 +1,72 @@
 // 클래스 리액트 컴포넌트
 // 클래스가 리액트 엘리먼트를 렌더링
+
+// class 구문(syntax)
+// class 클래스이름 {}
+
+// 리액트의 컴포넌트 클래스를 작성하려면?
+// 리액트.컴포넌트 클래스(super)를 확장한 서브 클래스 생성
+class AppButton extends React.Component {
+  // 생성자(constructor) 함수
+  constructor(props) {
+    super(props);
+  }
+
+  // 렌더 메서드
+  render() {
+    // 리액트 엘리먼트 반환
+    return React.createElement(
+      "button",
+      {
+        type: this.props.type,
+        className: this.props.className,
+        disabled: this.props.disabled,
+      },
+      this.props.children
+    );
+  }
+}
+// console.log(AppButton)
+
+// 리액트에서는 이렇게 사용하면 안됨 ❌
+// JavaScript에서 클래스로부터 인스턴스를 생성하려면?
+// const appButton = new AppButton()
+// console.log(appButton)
+
+// 리액트에서는 이렇게 사용해야 함 ✅
+// 리액트의 방식으로 컴포넌트로부터 엘리먼트 생성
+const disabledSubmitButton = React.createElement(
+  AppButton,
+  { disabled: true, type: "submit", className: "submit-button" },
+  "폼 제출"
+);
+
+// console.log(disabledSubmitButton)
+
+const enabledNormalButton = React.createElement(
+  AppButton,
+  { disabled: false, type: "button", className: "normal-button" },
+  "표시/감춤"
+);
+
+// console.log(enabledNormalButton)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      { className: "app" },
+      disabledSubmitButton,
+      enabledNormalButton
+    );
+  }
+}
+
+// ReactDOM을 사용해 실제 DOM에 렌더링
+ReactDOM.createRoot(document.getElementById("container")).render(
+  React.createElement(App)
+);
