bdc2cf69a956d6becfad3f643fc3f9bc372b48ab
wjinss <aastu4343@gmail.com>
2025-08-13 10:23:17 +0900

    learn. react.Fragment

--- 04-jsx-syntax-extension/src/components/output.js
+++ 04-jsx-syntax-extension/src/components/output.js
@@ -2,8 +2,9 @@ export default function Output(props) {
   const classNames = `output ${props.isAnimate ? "is-animate" : ""}`.trim();
 
   return React.createElement(
-    "output",
-    { className: classNames },
-    props.children
+    React.Fragment,
+    {},
+    React.createElement("output", { className: classNames }, props.children),
+    React.createElement("output", { className: classNames }, props.children + 1)
   );
 }
