7a2cd2c3d3e4fecec4c79de86b1a8d4da2dcd76a
wjinss <aastu4343@gmail.com>
2025-08-18 14:57:16 +0900

    learn. 상태 있는/없는 컴포넌트 구성 - 상태 있는 컴포넌트 (class) - 상태 없는 컴포넌트 (functional)

---
+++ 10-stateful-component 2/src/components/accordion/accordin-list.jsx
@@ -0,0 +1,55 @@
+import accordionData from './data.json'
+import { Component } from 'react'
+import AccordionItem from './accordion-item'
+
+// 클래스 컴포넌트
+export default class AccordionList extends Component {
+  // 상태 선언
+  // 클래스 필드 구문 활용
+  state = {
+    activeIndex: 0,
+  }
+
+  // 이벤트 핸들러
+  handleActiveIndex = (nextActiveIndex) => {
+    // 상태 업데이트
+    this.setState(
+      {
+        activeIndex: nextActiveIndex,
+      },
+      () => {
+        console.log(this.state.activeIndex)
+      }
+    )
+  }
+
+  render() {
+    const { activeIndex } = this.state
+
+    return (
+      <section>
+        <h2>
+          자주 묻는 질문
+          <img
+            src="/assets/tutor@2x.png"
+            alt="피그마 튜터"
+            width={43}
+            height={43}
+          />
+        </h2>
+        <dl>
+          {accordionData.map((accordionItem, index) => (
+            <AccordionItem
+              key={accordionItem.id}
+              question={accordionItem.question}
+              answer={accordionItem.answer}
+              isOpen={activeIndex === index}
+              index={index}
+              onActive={this.handleActiveIndex}
+            />
+          ))}
+        </dl>
+      </section>
+    )
+  }
+}
