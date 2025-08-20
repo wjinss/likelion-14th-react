edc3807945718a253d3da9d1dd62b4c1d7f1581c
wjinss <aastu4343@gmail.com>
2025-08-20 14:37:45 +0900

    learn.  useImmer 훅을 사용해 복잡하게 중첩된 상태를 관리

--- 13-manage-array-state/src/app.jsx
+++ 13-manage-array-state/src/app.jsx
@@ -1,5 +1,13 @@
+import { LearnSection } from './components'
 import ManageArrayState from './components/manage-array-state'
+import ManageArrayStateWithImmer from './components/manage-array-state/index-with-immer'
 
 export default function App() {
-  return <ManageArrayState />
+  return (
+    <LearnSection title="배열 상태 관리">
+      <ManageArrayStateWithImmer />
+      <hr />
+      <ManageArrayState />
+    </LearnSection>
+  )
 }
