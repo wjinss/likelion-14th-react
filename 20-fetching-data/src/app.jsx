2cbe4abcea520c1b297fa8a07ed253ee5f3969d1
wjinss <aastu4343@gmail.com>
2025-08-27 13:29:11 +0900

    learn. 컴포넌트 추출 (관심사의 분리) - 상위 컴포넌트의 id 속성이 변경되면 포함한 모든 컴포넌트 리렌더링 트리거 - 상위 컴포넌트의 id 속성 값이 변경되었음을 감지해 부수효과(서버에 데이터 가져오기) 다시 실행 - 상태 변경에 따라 화면 업데이트 (리액트 담당) - 더 나은 사용자 경험을 위한 노력 (프론트엔드 개발자 담당)

--- 20-fetching-data/src/app.jsx
+++ 20-fetching-data/src/app.jsx
@@ -1,13 +1,57 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useId, useState } from 'react'
 import { LearnSection } from '@/components'
 import { wait } from './utils'
-
-const API_URL = 'https://jsonplaceholder.typicode.com/albums/10'
 
 export default function App() {
   console.log('App 렌더링')
 
   const [key, setKey] = useState(0)
+
+  const albumInputId = useId()
+  const [albumId, setAlbumId] = useState(1)
+  const isAlbumInputDisabled = albumId === 1 || albumId === 100
+
+  return (
+    <LearnSection title="데이터 가져오기(fetching data)" showTitle>
+      <div role="group" className="my-5">
+        <label htmlFor={albumInputId} className="mr-2">
+          앨범 ID
+        </label>
+        <input
+          type="number"
+          className="input disabled:cursor-not-allowed"
+          aria-disabled={isAlbumInputDisabled}
+          id={albumInputId}
+          value={albumId}
+          onChange={(e) => {
+            const nextAlbumId = Number(e.target.value)
+            setAlbumId(nextAlbumId)
+          }}
+          min={1}
+          max={100}
+        />
+      </div>
+
+      <div role="group" className="mt-5">
+        <button
+          type="button"
+          className="button"
+          onClick={() => setKey((k) => k + 1)}
+        >
+          렌더링 키 변경
+        </button>
+        <output>렌더링 키: {key}</output>
+      </div>
+
+      <Album id={albumId} />
+    </LearnSection>
+  )
+}
+
+const ALBUM_API_URL = 'https://jsonplaceholder.typicode.com/albums'
+
+function Album({ id }) {
+  console.log(`Album ${id} 렌더링`)
 
   // 데이터 가져오기(fetching data) 상태 관리
   // 1. 로딩(loading | pending)
@@ -17,7 +61,8 @@ export default function App() {
   // 3. 데이터(data)
   const [data, setData] = useState(null)
 
-  // 부수 효과
+  // 부수 효과 관리
+  // 상위 컴포넌트에서 전달된 id 속성이 변경되면 다시 이펙트 함수 실행
   useEffect(() => {
     // 로딩 상태 전환
     setLoading(true)
@@ -26,10 +71,9 @@ export default function App() {
 
     // 리액트 렌더링(동기 방식으로 작동)과
     // 무관한 서버에서 데이터 가져오기 코드
-    fetch(API_URL)
+    fetch(ALBUM_API_URL + '/' + id)
       .then(async (response) => {
-        await wait()
-
+        await wait(0.4)
         if (response.ok) {
           return response.json()
         }
@@ -53,7 +97,7 @@ export default function App() {
         // 로딩 상태 전환
         setLoading(false)
       })
-  }, [])
+  }, [id])
 
   // 상태 관리
   // console.log({ loading, error, data })
@@ -63,42 +107,33 @@ export default function App() {
   // - 조건식 사용
   // - JSX 내부에 조건식 사용
 
-  return (
-    <LearnSection title="데이터 가져오기(fetching data)" showTitle>
-      {loading && (
-        <p
-          role="status"
-          aria-live="polite"
-          className="text-indigo-300 font-semibold text-2xl"
-        >
-          로딩 중...
-        </p>
-      )}
-      {error && (
-        <p
-          role="alert"
-          aria-live="assertive"
-          className="text-red-600 font-semibold text-2xl"
-        >
-          오류 발생!! {error.message}
-        </p>
-      )}
-      {data && (
-        <p className="text-indigo-600 font-semibold text-2xl">
-          앨범 타이틀 : {data?.id ?? 0} | {data?.title ?? 'Album Title'}
-        </p>
-      )}
+  if (loading) {
+    return (
+      <p
+        role="status"
+        aria-live="polite"
+        className="text-indigo-300 font-semibold text-2xl"
+      >
+        로딩 중...
+      </p>
+    )
+  }
 
-      <div role="group" className="mt-5">
-        <button
-          type="button"
-          className="button"
-          onClick={() => setKey((k) => k + 1)}
-        >
-          렌더링 키 변경
-        </button>
-        <output>렌더링 키: {key}</output>
-      </div>
-    </LearnSection>
+  if (error) {
+    return (
+      <p
+        role="alert"
+        aria-live="assertive"
+        className="text-red-600 font-semibold text-2xl"
+      >
+        오류 발생!! {error.message}
+      </p>
+    )
+  }
+
+  return (
+    <p className="text-indigo-600 font-semibold text-2xl">
+      앨범 타이틀 : {data?.id ?? 0} | {data?.title ?? 'Album Title'}
+    </p>
   )
 }
