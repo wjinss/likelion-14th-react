ba02ace7ccdf2f9291307058ac3b1aea110491b1
wjinss <aastu4343@gmail.com>
2025-08-28 11:52:39 +0900

    add. 23-simple-todo-list 실습 폴더

--- 23-simple-todo-list/src/components/todo-list/index.jsx
+++ 23-simple-todo-list/src/components/todo-list/index.jsx
@@ -1,4 +1,14 @@
+import { useState } from 'react'
+import { useImmer } from 'use-immer'
+
 export default function SimpleTodoList() {
+  const [doit, newDoit] = useState('')
+  const handleChange = (e) => newDoit(e.target.value)
+
+  const [list] = useImmer([
+    { id: crypto.randomUUID(), doit: '보육원에 가서 점심 사주기', done: false },
+  ])
+
   return (
     <div className="container">
       <section>
@@ -6,7 +16,12 @@ export default function SimpleTodoList() {
         <form className="new-todo-form">
           <div role="group" className="form-control grow">
             <label htmlFor="todo-input">새로운 할 일</label>
-            <input type="text" id="todo-input" />
+            <input
+              type="text"
+              id="todo-input"
+              value={doit}
+              onChange={handleChange}
+            />
           </div>
           <button className="button" type="submit">
             추가
@@ -16,14 +31,18 @@ export default function SimpleTodoList() {
       <section>
         <h2 className="sr-only">할 일 목록</h2>
         <ul className="todo-list">
-          <li className="list-item">
-            <div className="form-control row">
-              <input id="todo-item-cisdk" type="checkbox" />
-              <label htmlFor="todo-item-cisdk" className="list-item-label">
-                할 일 1
-              </label>
-            </div>
-          </li>
+          {list.map(({ id, doit, done }) => {
+            return (
+              <li key={id} className="list-item">
+                <div className="form-control row">
+                  <input id="todo-item-cisdk" type="checkbox" checked={done} />
+                  <label htmlFor="todo-item-cisdk" className="list-item-label">
+                    {doit}
+                  </label>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </section>
     </div>
