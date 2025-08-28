904b46ca13c91cf5568dd40bf28a0ad545386750
wjinss <aastu4343@gmail.com>
2025-08-28 16:57:40 +0900

    fix. 경로 수정

--- 24-tic-tac-toe/src/app.jsx
+++ 24-tic-tac-toe/src/app.jsx
@@ -1,10 +1,9 @@
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
-import TicTacToe from './components/tic-tac-toe/tic-tac-toe'
+import TicTacToe from './components/tic-tac-toe'
 
 export default function App() {
   return (
-    <LearnSection title="틱택토 게임">
+    <LearnSection title="틱택토 게임 구현">
       <TicTacToe />
     </LearnSection>
   )
