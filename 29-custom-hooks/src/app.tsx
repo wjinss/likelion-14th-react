e2d1be384aea5f11bcd2a6f1bb93b4adbe3a09ce
wjinss <aastu4343@gmail.com>
2025-09-02 13:31:55 +0900

    learn. 여러 컴포너트에서 중복된 로직 사용됨을 구현 - toggle 상태 - input 상태

--- 29-custom-hooks/src/app.tsx
+++ 29-custom-hooks/src/app.tsx
@@ -1,10 +1,99 @@
+import { type ChangeEvent, useState } from 'react'
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo'
+import { tw } from './utils'
 
 export default function App() {
+  // [1] 토글 상태
+  // 테마(theme)
+  const [darkTheme, setDarkTheme] = useState<boolean>(false)
+  // 체크박스 기능: 테마 토글 (checkbox)
+  const handleChecked = (e: ChangeEvent<HTMLInputElement>) => {
+    setDarkTheme(e.target.checked)
+  }
+
+  const themeClassNames = darkTheme ? 'bg-slate-950 text-white' : ''
+  const checkeboxLabel = darkTheme ? '라이트 테마 전환' : '다크 테마 전환'
+
+  // [2] 인풋 상태
+  const [num, setNum] = useState<number>(0)
+  const handleChangeNum = (e: ChangeEvent<HTMLInputElement>) => {
+    setNum(Number(e.target.value))
+  }
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection
+      title="사용자 정의 훅 (Custom Hooks)"
+      showTitle
+      className={tw('p-10 h-screen', themeClassNames)}
+    >
+      <div role="group" className="flex gap-1 items-center">
+        <input
+          type="checkbox"
+          id="theme-checkbox"
+          checked={darkTheme}
+          onChange={handleChecked}
+        />
+        <label htmlFor="theme-checkbox">{checkeboxLabel}</label>
+      </div>
+      <div role="group" className="flex flex-col gap-2 my-4">
+        <label htmlFor="number-input">숫자</label>
+        <input
+          type="number"
+          value={num}
+          onChange={handleChangeNum}
+          id="number-input"
+          className="my-2"
+          min={0}
+          max={10}
+        />
+      </div>
+      <output>{0}</output>
+      <CustomHookDemo />
     </LearnSection>
+  )
+}
+
+function CustomHookDemo() {
+  // [관심사] 토글 상태 관리
+  // 상태
+  const [toggleState, setToggleState] = useState<boolean>(true)
+  // 상태 업데이트
+  const handleChangeLanguage = () => {
+    setToggleState((t) => !t)
+  }
+
+  const language = toggleState ? 'ko' : 'en'
+  const isKorean = language.includes('ko')
+
+  // [관심사] 인풋 상태 관리
+  // 상태
+  const [name, setName] = useState<string>('')
+  // 상태 업데이트
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+
+  return (
+    <>
+      <div role="group" className="flex flex-col gap-2 my-4">
+        <label htmlFor="user-input">이름</label>
+        <input
+          name={name}
+          onChange={handleChange}
+          type="text"
+          id="user-input"
+          className="my-2"
+        />
+        <output>{name || '이름 값 출력'}</output>
+      </div>
+      <button
+        type="button"
+        className="cursor-pointer bg-black text-white p-2"
+        lang={isKorean ? 'en' : 'ko'}
+        onClick={handleChangeLanguage}
+      >
+        {isKorean ? 'change english' : '한국어 전환'}
+      </button>
+    </>
   )
 }
--- 29-custom-hooks/src/components/learn-section.tsx
+++ 29-custom-hooks/src/components/learn-section.tsx
@@ -7,8 +7,14 @@ export default function LearnSection(props: Props) {
   const { title, showTitle = false, children, ...restProps } = props
 
   return (
-    <section {...restProps}>
-      <h1 className={tw([showTitle || 'sr-only'])}>{title}</h1>
+    <section className={tw('p-5')} {...restProps}>
+      <h1
+        className={tw('text-2xl text-inherit font-semibold mb-2', [
+          showTitle || 'sr-only',
+        ])}
+      >
+        {title}
+      </h1>
       {children}
     </section>
   )
