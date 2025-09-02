9f49ea4f204c994e21345a7f23f180ece1b1c815
wjinss <aastu4343@gmail.com>
2025-09-02 17:31:06 +0900

    lear. 커스텀 훅 useQuery 작성

--- 29-custom-hooks/src/@types/global.d.ts
+++ 29-custom-hooks/src/@types/global.d.ts
@@ -1,1 +1,9 @@
-type Timeout = ReturnType<typeof setTimeout> | number | undefined
+export type Timeout = ReturnType<typeof setTimeout> | number | undefined
+
+export type Status = 'idle' | 'pending' | 'resolved' | 'rejected'
+
+export interface State<Type> {
+  status: Status
+  error: null | Error
+  data: null | Type
+}
--- 29-custom-hooks/src/hooks/index.ts
+++ 29-custom-hooks/src/hooks/index.ts
@@ -4,3 +4,4 @@
 export { default as useToggleState } from './use-toggle'
 export { default as useInput } from './use-input'
 export { default as useArray } from './use-array'
+export { default as useQuery } from './use-query'
