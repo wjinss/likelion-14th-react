088335b1459b118966b82655279def531e3354b3
wjinss <aastu4343@gmail.com>
2025-09-08 15:30:12 +0900

    learn. 모달 다이얼로그 (커스텀 편) 2 - 열기/닫기 기능 구현 (상태 제어) - 사용성/접근성 고려 사용자 편의를 향상

--- 35-modal-dialog/src/components/dialog/custom-modal-dialog.tsx
+++ 35-modal-dialog/src/components/dialog/custom-modal-dialog.tsx
@@ -1,23 +1,27 @@
 // Modal Dialog 컴포넌트
 // 1. Custom Modal Dialog: <div role="dialog" aria-modal="true">
 // 2. Native Modal Dialog: <dialog aria-modal="true">
-import { type PropsWithChildren, useId } from 'react'
+import { type MouseEvent, type PropsWithChildren, useId, useRef } from 'react'
 import { createPortal } from 'react-dom'
 import { XCircleIcon } from 'lucide-react'
 import { tw } from '@/utils'
 
 type Props = PropsWithChildren<{
   open?: boolean
+  onClose?: () => void
   title?: string
   describe?: string
 }>
 
 export default function CustomModalDialog({
   open = false,
+  onClose,
   title,
   describe,
   children,
 }: Props) {
+  const dialogDimRef = useRef<HTMLDivElement>(null)
+
   const dialogId = useId()
   const titleId = `${dialogId}-title`
   const describeId = `${dialogId}-describe`
@@ -25,8 +29,17 @@ export default function CustomModalDialog({
   const portalContainer = document.getElementById('modal-dialog-portal')
   if (!portalContainer) return null
 
+  const handleClose = (e: MouseEvent<HTMLDivElement>) => {
+    if (dialogDimRef.current === e.target) {
+      onClose?.()
+    }
+  }
+
   return createPortal(
     <div
+      ref={dialogDimRef}
+      role="presentation"
+      onClick={handleClose}
       className={tw(
         'fixed inset-0 z-10000',
         open ? 'flex' : 'hidden',
@@ -51,6 +64,7 @@ export default function CustomModalDialog({
           type="button"
           aria-label="다이얼로그 닫기"
           title="다이얼로그 닫기"
+          onClick={onClose}
           className={tw(
             'cursor-pointer',
             'absolute -top-2.5 -right-2.5 bg-black text-white rounded-full'
