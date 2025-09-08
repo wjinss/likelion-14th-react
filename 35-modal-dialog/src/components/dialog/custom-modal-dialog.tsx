f29cdc11e17dc5f999d0becd31a94f5c372236c9
wjinss <aastu4343@gmail.com>
2025-09-08 16:37:26 +0900

    learn. 모달 다이얼로그 (커스텀 편) 3 - 다이얼로그 내부에서 탭 이동 가능한(Tabbable) 요소 수집
    learn. 모달 다이얼로그 (커스텀 편) 4
    - 포커스 트랩 기능 구현
    - 탈출(Escape) 기능 구현
    - 탈출 또는 닫기 기능 작동 시, 다이얼로그를 열었던 요소에 초점 복구

--- 35-modal-dialog/src/components/dialog/custom-modal-dialog.tsx
+++ 35-modal-dialog/src/components/dialog/custom-modal-dialog.tsx
@@ -1,10 +1,16 @@
 // Modal Dialog 컴포넌트
 // 1. Custom Modal Dialog: <div role="dialog" aria-modal="true">
 // 2. Native Modal Dialog: <dialog aria-modal="true">
-import { type MouseEvent, type PropsWithChildren, useId, useRef } from 'react'
+import {
+  type MouseEvent,
+  type PropsWithChildren,
+  useEffect,
+  useId,
+  useRef,
+} from 'react'
 import { createPortal } from 'react-dom'
 import { XCircleIcon } from 'lucide-react'
-import { tw } from '@/utils'
+import { tabbableSelector, tw } from '@/utils'
 
 type Props = PropsWithChildren<{
   open?: boolean
@@ -21,10 +27,84 @@ export default function CustomModalDialog({
   children,
 }: Props) {
   const dialogDimRef = useRef<HTMLDivElement>(null)
+  const dialogRef = useRef<HTMLDivElement>(null)
+  const opnnerRef = useRef<HTMLElement>(null)
 
   const dialogId = useId()
   const titleId = `${dialogId}-title`
   const describeId = `${dialogId}-describe`
+
+  useEffect(() => {
+    const dialog = dialogRef.current
+    if (!open || !dialog) return
+
+    // 모달 다이얼로그를 연 어떤 요소를 RefObject를 사용해 기억(memo)
+    opnnerRef.current = document.activeElement as HTMLElement
+
+    // 모달이 열리면 모달 안에서 탭키로 탐색 가능한(tabbale) 요소들을 수집
+    const tabbables = [...dialog.querySelectorAll(tabbableSelector)]
+    const isExistTabbables = tabbables.length > 0
+
+    // 첫 번째 탭 키로 이동 가능한 요소에 초점 이동 설정
+    const focusingFirstTabbable = () => {
+      // 탭 가능한 요소가 있으면 실행
+      if (isExistTabbables) {
+        // 첫 번째 탭 가능한 요소
+        const firtsTabbable = tabbables.at(0)
+        if (firtsTabbable) {
+          ;(firtsTabbable as HTMLElement).focus()
+        }
+      }
+    }
+
+    focusingFirstTabbable()
+
+    // 포커스 트랩(focus-trap)
+    // - 탭 가능한 요소들 중 첫 번째 요소에서 shift + 탭 키를 누르면
+    // 마지막 요소로 초점 이동(브라우저의 기본 작동 방지)
+    // - 탭 가능한 요소들 중 마지막 요소에서 탭 키를 누르면
+    // 첫 번째 요소로 초점 이동(브라우저의 기본 작동 방지)
+    // - 탈출(escape) 키를 누르면 모달 다이얼로그가 닫혀야 하고,
+    // 모달 다이얼로그를 트리거한 오프너(openner) 요소에 초점을 돌려줘야 함
+    const handleFocusTrap = (e: globalThis.KeyboardEvent) => {
+      // 포커스 가능한 요소가 없으면 종료
+      if (!isExistTabbables) return
+
+      const { key, shiftKey } = e
+      const { activeElement } = document
+      //tabbables.length > 0로 인해 무조건 요소가 있는 것으로간주
+      const firtsTabbable = tabbables.at(0) as HTMLElement
+      const lastTabbable = tabbables.at(-1) as HTMLElement
+
+      if (key === 'Escape') {
+        onClose?.()
+        opnnerRef.current?.focus()
+        return
+      }
+
+      if (key === 'Tab') {
+        if (shiftKey && activeElement === firtsTabbable) {
+          // - 탭 가능한 요소들 중 첫 번째 요소에서 shift + 탭 키를 누르면
+          // 마지막 요소로 초점 이동(브라우저의 기본 작동 방지)
+          console.log('첫번째 이동 가능한 요소에서 슆 + 탭 키 누름')
+          e.preventDefault()
+          lastTabbable.focus()
+        } else if (!shiftKey && activeElement === lastTabbable) {
+          // - 탭 가능한 요소들 중 마지막 요소에서 탭 키를 누르면
+          // 첫 번째 요소로 초점 이동(브라우저의 기본 작동 방지)
+          e.preventDefault()
+          console.log('마지막 이동 가능한 요소에서 탭 키 누름')
+          firtsTabbable.focus()
+        }
+      }
+    }
+
+    dialog.addEventListener('keydown', handleFocusTrap)
+
+    return () => {
+      dialog.removeEventListener('keydown', handleFocusTrap)
+    }
+  }, [open, onClose])
 
   const portalContainer = document.getElementById('modal-dialog-portal')
   if (!portalContainer) return null
@@ -48,6 +128,7 @@ export default function CustomModalDialog({
       )}
     >
       <div
+        ref={dialogRef}
         role="dialog"
         aria-modal="true"
         aria-labelledby={titleId}
