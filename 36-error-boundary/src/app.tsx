950a85fecb8c6a72602de26333b699ec9137decf
wjinss <aastu4343@gmail.com>
2025-09-09 14:16:32 +0900

    learn. ErrorBoundary 컴포넌트 4 - react-error-boundary 라이브러리 설치 및 활용 - fallback 속성 - fallbackReder 속성 - FallbackComponent 속성

--- 36-error-boundary/src/app.tsx
+++ 36-error-boundary/src/app.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { LearnSection } from '@/components'
 import RandomCountUp from './demo'
-import ErrorBoundary from './error-boundar'
 
 export default function App() {
   return (
@@ -28,7 +29,19 @@ export default function App() {
         <RandomCountUp />
       </ErrorBoundary>
 
-      <ErrorBoundary>
+      <ErrorBoundary
+        // fallbackRender={errorRender}
+        FallbackComponent={({ error, resetErrorBoundary }) => {
+          return (
+            <div role="alert">
+              {error.message}{' '}
+              <button type="button" onClick={resetErrorBoundary}>
+                error recovery
+              </button>
+            </div>
+          )
+        }}
+      >
         <Child />
       </ErrorBoundary>
     </LearnSection>
@@ -36,5 +49,31 @@ export default function App() {
 }
 
 function Child() {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments/1001')
+      .then((response) => {
+        if (!response.ok) {
+          return Promise.reject('오류 발생!')
+        }
+        response.json()
+      })
+      .then(console.log)
+      .catch((error) => console.error(error))
+  })
+
+  // throw new Error('Child 컴포넌트 렌더링 시 알 수 없는 오류 포착')
+
   return <div>Child</div>
 }
+
+const errorRender = ({ error, resetErrorBoundary }) => (
+  <div role="alert" className="text-red-500">
+    <h2>Child 오류 발생</h2>
+    <p>
+      {error.message}
+      <button type="button" className="button" onClick={resetErrorBoundary}>
+        reset
+      </button>
+    </p>
+  </div>
+)
