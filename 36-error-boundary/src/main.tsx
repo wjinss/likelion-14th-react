28abc2c6a71b39f8909ecfc1043587123e85ef98
wjinss <aastu4343@gmail.com>
2025-09-09 11:16:22 +0900

    learn. 오류 경계(Error Boundary) - 렌더링 에러 시뮬레이션 - 문제 해결을 위해서 ErroryBoundary 컴포넌트가 필요함

--- 36-error-boundary/src/main.tsx
+++ 36-error-boundary/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
-import { ErrorBoundary } from '@/components'
 import '@/styles/main.css'
 import App from './app'
 
@@ -10,9 +9,7 @@ if (!root) throw new Error('문서에 #root 요소가 존재하지 않습니다.
 
 createRoot(root).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-      <Toaster position="bottom-center" />
-    </ErrorBoundary>
+    <App />
+    <Toaster position="bottom-center" />
   </StrictMode>
 )
