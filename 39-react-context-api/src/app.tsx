d2c3d19f9c8c96842dcc8fe06a484b0a20e26435
wjinss <aastu4343@gmail.com>
2025-09-10 16:16:59 +0900

    learn. 컨텍스트 API로 상태 공유 3 - AuthContext 생성 - AuthContext.Provider로 값 공급 - useContext 훅을 사용해 컨텍스트 값 사용

--- 39-react-context-api/src/app.tsx
+++ 39-react-context-api/src/app.tsx
@@ -1,74 +1,56 @@
-import { createContext, useState } from 'react'
-import { LearnSection } from '@/components'
-import RandomCountUp from './components/random-count-up'
+import { createContext, useCallback, useContext, useState } from 'react'
+import { LearnSection } from './components'
 
-type ThemeContextValue = 'light' | 'dark'
+type User = {
+  email: string
+  name: string
+  accessToken: string
+} | null
 
-const defaultThemeValue: ThemeContextValue = 'light'
-
-const themeConfig = {
-  dark: {
-    backgroundColor: 'darkblue',
-    color: 'white',
-  },
-  light: {
-    backgroundColor: 'white',
-    color: 'darkgray',
-  },
+interface AuthContextValue {
+  user: User
+  logIn?: () => void
+  logOut?: () => void
 }
 
-const ThemeContext = createContext<ThemeContextValue>(defaultThemeValue)
-ThemeContext.displayName = 'ThemeContext'
-
-// --------------------------------------------------------------------------
-
-// 컨텍스트 값(value)의 타입 정의
-interface GlobalStateValue {
-  mood: string
-  currentYear: number
-}
+const AuthContext = createContext<AuthContextValue>({
+  user: null,
+})
 
 // Context 생성
 // Context.displayName 이름 지정
 // Context.Provider[value] 값 공급
 // Context.Consumer(render props) 값 수요
 
-// 1. 컨텍스트 생성하기
-export const GlobalStateContext = createContext<null | GlobalStateValue>(null)
-// 컨텍스트 이름 지정하기
-GlobalStateContext.displayName = 'GlobalStateContext'
+AuthContext.displayName = 'AuthContext'
 
 export default function App() {
-  const [mood] = useState('기분 많이 좋음')
-  const [currentYear] = useState(new Date().getFullYear())
+  const [authUser, setAuthUser] = useState<User>(null)
 
-  const [themeMode, setThemeMode] = useState<ThemeContextValue>('light')
+  const logIn = useCallback(() => {
+    setAuthUser({
+      email: 'user@example.com',
+      name: '테스터',
+      accessToken: crypto.randomUUID(),
+    })
+  }, [])
+
+  const logOut = useCallback(() => {
+    setAuthUser(null)
+  }, [])
+
+  const authValue: AuthContextValue = { user: authUser, logIn, logOut }
 
   return (
-    // 2. 컨텍스트 공급(제공, provider)하기
-    <ThemeContext.Provider value={themeMode}>
-      <GlobalStateContext.Provider value={{ mood, currentYear }}>
-        <button
-          type="button"
-          className="button m-10"
-          onClick={() => {
-            setThemeMode((tm) => (tm.includes('light') ? 'dark' : 'light'))
-          }}
-        >
-          {themeMode.includes('light') ? 'dark' : 'light'}
-        </button>
-        <LearnSection
-          title="리액트 컨텍스트를 사용해 데이터 공유"
-          showTitle
-          className="text-indigo-600"
-        >
-          <div className="scale-80 flex gap-10 justify-center items-center p-20 border-8 border-indigo-600 text-indigo-600">
-            <h2 className="text-inherit">App</h2>
-            <GrandParent />
-          </div>
-        </LearnSection>
-      </GlobalStateContext.Provider>
-    </ThemeContext.Provider>
+    <AuthContext.Provider value={authValue}>
+      <LearnSection
+        title="리액트 컨텍스트를 사용해 데이터 공유"
+        showTitle
+        className="text-indigo-800 p-10 scale-100"
+      >
+        <GrandParent />
+      </LearnSection>
+    </AuthContext.Provider>
   )
 }
 
@@ -76,56 +58,70 @@ function GrandParent() {
   return (
     <div className="flex gap-10 justify-center items-center p-20 border-8 border-indigo-600 text-indigo-600">
       <h2 className="text-inherit">Grand Parent</h2>
+      <Navigation />
       <Parent />
     </div>
   )
 }
 
-function Parent() {
-  return (
-    <ThemeContext.Consumer>
-      {(themeContextValue) => {
-        const currentTheme = themeConfig[themeContextValue]
+function Navigation() {
+  const { logIn, user, logOut } = useContext(AuthContext)
 
-        return (
-          <div
-            style={currentTheme}
-            className="flex gap-10 justify-center items-center p-20 border-8 border-indigo-600 text-indigo-600"
-          >
-            <h2 className="text-inherit">Parent</h2>
-            <Child />
-          </div>
-        )
-      }}
-    </ThemeContext.Consumer>
+  return (
+    <nav>
+      <ul className="list-none pl-0">
+        <li>
+          {!user ? (
+            <a
+              href="/login"
+              className="no-underline button"
+              onClick={(e) => {
+                e.preventDefault()
+                logIn?.()
+              }}
+            >
+              로그인
+            </a>
+          ) : (
+            <a
+              href="/logout"
+              className="no-underline button"
+              onClick={(e) => {
+                e.preventDefault()
+                logOut?.()
+              }}
+            >
+              로그아웃
+            </a>
+          )}
+        </li>
+      </ul>
+    </nav>
   )
 }
 
-function Child() {
+function Parent() {
   return (
     <div className="flex gap-10 justify-center items-center p-20 border-8 border-indigo-600 text-indigo-600">
-      <h2 className="text-inherit">Child</h2>
-      <GrandChild />
+      <h2 className="text-inherit">Parent</h2>
+      <Child />
     </div>
   )
 }
 
-function GrandChild() {
-  return (
-    <ThemeContext.Consumer>
-      {(themeContextValue) => {
-        const currentTheme = themeConfig[themeContextValue]
+function Child() {
+  const { user } = useContext(AuthContext)
 
-        return (
-          <div
-            style={currentTheme}
-            className="flex gap-10 justify-center items-center p-20 border-8 border-indigo-600 text-indigo-600"
-          >
-            <h2 className="text-inherit">Grand Child</h2>
-            {/* <RandomCountUp /> */}
-          </div>
-        )
-      }}
-    </ThemeContext.Consumer>
+  return (
+    <div className="flex flex-col gap-10 justify-center items-center p-20 border-8 border-indigo-600 text-indigo-600">
+      <h2 className="text-inherit">Child</h2>
+      {user ? (
+        <p>
+          {user.name} | {user.email} | {user.accessToken}
+        </p>
+      ) : (
+        <p>로그인이 필요합니다.</p>
+      )}
+    </div>
   )
 }
