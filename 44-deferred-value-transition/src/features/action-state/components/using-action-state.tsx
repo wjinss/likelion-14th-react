0de9995692d1f4c14d42558747fbc3c680894b29
wjinss <aastu4343@gmail.com>
2025-09-18 09:40:15 +0900

    learn. 리액트 19 액션(Actions) - useActionState 훅 - useFormStatus 훅 (react-dom)

--- 44-deferred-value-transition/src/features/action-state/components/using-action-state.tsx
+++ 44-deferred-value-transition/src/features/action-state/components/using-action-state.tsx
@@ -1,26 +1,63 @@
-import { useActionState, useId } from 'react'
+import {
+  useActionState,
+  useId,
+  // type FormEvent,
+  // useState,
+  // useTransition,
+} from 'react'
+import { useFormStatus } from 'react-dom'
 import { tw, wait } from '@/utils'
 
 export default function UsingActionState() {
   const id = useId()
 
-  // 이전에 작성된 코드
+  // --------------------------------------------------------------------------
+  // 리액트에서 이전에 사용되던 코드
+
+  // const [data, setData] = useState<string>('1')
+  // const [isPending, setIsPending] = useState<boolean>(false)
+
+  // const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault()
+  //   setIsPending(true)
+
+  //   const form = e.currentTarget
+  //   const formData = new FormData(form)
+  //   const age = formData.get('age') as string
+
+  //   await wait(1)
+
+  //   setData(age)
+  //   setIsPending(false)
+  //   form.reset()
+  // }
+
+  // --------------------------------------------------------------------------
+  // [실습]
+
+  // <form> 요소의 action 속성 + useState + useTransition 코드
+
   // const [data, setData] = useState<string>('1')
   // const [isPending, startTransition] = useTransition()
 
   // const formAction = async (formData: FormData) => {
+  //   const age = formData.get('age') as string
   //   startTransition(async () => {
-  //     const age = formData.get('age') as string
-  //     await wait(1)
+  //     await wait(1.2)
   //     setData(age)
   //   })
   // }
 
-  // useActionState 훅을 사용한 코드
-  const [age, formAction, isPending] = useActionState(
-    async (_state: string, formData: FormData) => {
-      await wait(1)
-      return formData.get('age') as string
+  // --------------------------------------------------------------------------
+  // [실습]
+  // useActionState 코드
+  // 폼 상태, 폼 액션(함수), 팬딩 상태
+
+  const [data, formAction, isPending] = useActionState(
+    async (_prevData: string, formData: FormData) => {
+      const age = formData.get('age') as string
+      await wait(1.2)
+      return age
     },
     '1'
   )
@@ -28,6 +65,7 @@ export default function UsingActionState() {
   return (
     <form
       action={formAction}
+      // onSubmit={handleSubmit}
       className={tw('max-w-xs mt-2 p-6 bg-white rounded-lg shadow space-y-4')}
     >
       <label htmlFor={id} className="block text-slate-800 font-semibold">
@@ -38,32 +76,46 @@ export default function UsingActionState() {
         name="age"
         id={id}
         disabled={isPending}
-        className={tw(
-          'w-full px-3 py-2',
-          'border border-gray-300 rounded',
-          'focus:outline-none focus:ring-2 focus:ring-indigo-500'
-        )}
-        defaultValue={age}
+        defaultValue={data}
         min={1}
+        className={tw`
+          w-full px-3 py-2
+          border border-gray-300 rounded
+          focus:outline-none focus:ring-2 focus:ring-indigo-500
+        `}
       />
-      <button
-        type="submit"
-        disabled={isPending}
-        className={tw(
-          'w-full py-2 px-4 rounded font-bold transition',
-          isPending && 'bg-gray-300 text-gray-500 cursor-not-allowed',
-          isPending || 'bg-indigo-600 text-white hover:bg-indigo-700'
-        )}
-      >
-        {isPending ? '저장 중...' : '저장'}
-      </button>
-      {isPending || (
-        <div
-          className={tw('text-center text-sm font-semibold text-indigo-700')}
-        >
-          나이 = {age}
-        </div>
-      )}
+      <SubmitButton />
+      <PrintData data={data} />
     </form>
+  )
+}
+
+function PrintData({ data }: { data: string }) {
+  const { pending } = useFormStatus()
+
+  return (
+    pending || (
+      <div className={tw`text-center text-sm font-semibold text-indigo-700`}>
+        나이 = {data}
+      </div>
+    )
+  )
+}
+
+function SubmitButton() {
+  const { pending: isPending } = useFormStatus()
+
+  return (
+    <button
+      type="submit"
+      disabled={isPending}
+      className={tw(
+        'w-full py-2 px-4 rounded font-bold transition',
+        isPending && 'bg-gray-300 text-gray-500 cursor-not-allowed',
+        isPending || 'bg-indigo-600 text-white hover:bg-indigo-700'
+      )}
+    >
+      {isPending ? '저장 중...' : '저장'}
+    </button>
   )
 }
