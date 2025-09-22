df30187005d5a13086d15389100fadb12b917dd1
wjinss <aastu4343@gmail.com>
2025-09-22 17:25:50 +0900

    learn. 카카오 도서 API 요청/응답(API 최신화 전) - 카카오 디벨로프 가입/로그인 후 REST API 키 발급 활용 - 응답 타입 정의 활용

--- learn-next-js/05-loading-error/src/app/books/api.ts
+++ learn-next-js/05-loading-error/src/app/books/api.ts
@@ -5,17 +5,51 @@
 
 // --------------------------------------------------------------------------
 
+// import type { QuoteResponse } from './types'
+
+// // 데이터 가져오기 비동기 함수
+// export const fetchQuotes = async (): Promise<QuoteResponse['quotes']> => {
+//   // 서버 컴포넌트의 데이터 가져오기 (✅ 권장!!! 정적 렌더링 1회)
+//   const response = await fetch('https://dummyjson.com/quotes')
+//   const { quotes } = await response.json()
+
+//   // await wait(2)
+//   // throw new Error('도서 목록 가져오기에 실패했습니다.')
+
+//   return quotes
+// }
+
+// --------------------------------------------------------------------------
 import { wait } from '@/utils'
-import type { QuoteResponse } from './types'
+import type { KakaoBooksResponse } from './types'
+
+const headersList = {
+  'Content-Type': 'Application/json',
+  Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+}
+
+interface Options {
+  query: string
+}
 
 // 데이터 가져오기 비동기 함수
-export const fetchQuotes = async (): Promise<QuoteResponse['quotes']> => {
-  // 서버 컴포넌트의 데이터 가져오기 (✅ 권장!!! 정적 렌더링 1회)
-  const response = await fetch('https://dummyjson.com/quotes')
-  const { quotes } = await response.json()
+export const fetchKakaoBooks = async ({
+  query,
+}: Options): Promise<KakaoBooksResponse> => {
+  const encodedQuery = encodeURIComponent(query)
 
-  // await wait(2)
-  // throw new Error('도서 목록 가져오기에 실패했습니다.')
+  await wait()
 
-  return quotes
+  const response = await fetch(
+    `https://dapi.kakao.com/v3/search/book?query=${encodedQuery}`,
+    {
+      headers: headersList,
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error('카카오 도서 API에서 데이터 가져오기에 실패했습니다.')
+  }
+
+  return response.json()
 }
--- learn-next-js/05-loading-error/src/app/books/page.tsx
+++ learn-next-js/05-loading-error/src/app/books/page.tsx
@@ -1,7 +1,35 @@
-// import { use } from 'react'
+// // import { use } from 'react'
+// import type { Metadata } from 'next'
+// import { Section } from '@/components'
+// import { fetchQuotes } from './api'
+
+// export const metadata: Metadata = {
+//   title: '도서 목록',
+//   description: '런메이트(LearnMate)가 추천하는 도서 목록을 확인하세요.',
+// }
+
+// // 리액트 서버 컴포넌트
+// export default async function BooksPage() {
+//   const quotes = await fetchQuotes() // 데이터 가져온 후, 서버에서 렌더링
+
+//   return (
+//     <Section title="도서 목록 페이지">
+//       <p>도서 목록 페이지 방문</p>
+//       <ul className="flex flex-col gap-y-4">
+//         {quotes.map(({ id, quote }) => (
+//           <li key={id} className="p-2 border-1 border-slate-300">
+//             {quote}
+//           </li>
+//         ))}
+//       </ul>
+//     </Section>
+//   )
+// }
+// --------------------------------------------------------------------------
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from 'next'
 import { Section } from '@/components'
-import { fetchQuotes } from './api'
+import { fetchKakaoBooks } from './api'
 
 export const metadata: Metadata = {
   title: '도서 목록',
@@ -10,15 +38,18 @@ export const metadata: Metadata = {
 
 // 리액트 서버 컴포넌트
 export default async function BooksPage() {
-  const quotes = await fetchQuotes() // 데이터 가져온 후, 서버에서 렌더링
+  const { documents } = await fetchKakaoBooks({ query: '리액트' })
 
   return (
     <Section title="도서 목록 페이지">
       <p>도서 목록 페이지 방문</p>
-      <ul className="flex flex-col gap-y-4">
-        {quotes.map(({ id, quote }) => (
-          <li key={id} className="p-2 border-1 border-slate-300">
-            {quote}
+      <ul className="gap-y-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {documents.map(({ isbn, thumbnail, title, status, price }) => (
+          <li key={isbn} className="p-2 border-1 border-slate-300">
+            <img src={thumbnail} alt={title} width={120} height={120} />{' '}
+            <h3>{title}</h3>
+            <span>{price.toLocaleString()}</span>
+            <span> {status}</span>
           </li>
         ))}
       </ul>
