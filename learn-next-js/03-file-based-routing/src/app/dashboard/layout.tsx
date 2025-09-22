e4c2e1b9539f38cc2c45f4508aa9d071cb599069
wjinss <aastu4343@gmail.com>
2025-09-22 13:25:41 +0900

    learn. 커스텀 NavLink 컴포넌트 2 - exact 속성의 쓰임새 - disabled 비활성 상태 설정 - 중첩된 네비게이션 하위 목록(LNB)

--- learn-next-js/03-file-based-routing/src/app/dashboard/layout.tsx
+++ learn-next-js/03-file-based-routing/src/app/dashboard/layout.tsx
@@ -1,32 +1,30 @@
 import type { PropsWithChildren } from 'react'
 
-import { Link } from '@/components'
-
 export default function DashboardLayout({ children }: PropsWithChildren) {
   console.log('대시보드 레이아웃')
 
   return (
     <div className="bg-slate-200 flex-1">
-      <nav className="p-5">
+      {/* <nav className="p-5">
         <ul>
           <li>
-            <Link
+            <NavLink
               className="text-sky-600 hover:text-sky-900"
               href="/dashboard/profile"
             >
               대시보드 &gt; 프로필
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               className="text-sky-600 hover:text-sky-900"
               href="/dashboard/settings"
             >
               대시보드 &gt; 설정
-            </Link>
+            </NavLink>
           </li>
         </ul>
-      </nav>
+      </nav> */}
       {children}
     </div>
   )
--- learn-next-js/03-file-based-routing/src/app/layout.tsx
+++ learn-next-js/03-file-based-routing/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import '@/styles/main.css'
 import type { PropsWithChildren } from 'react'
 
-import { Link } from '@/components'
+import { NavLink } from '@/components'
+
+import { tw } from '../utils'
 
 export default function RootLayout({ children }: PropsWithChildren) {
   console.log('루트 레이아웃')
@@ -18,20 +20,50 @@ export default function RootLayout({ children }: PropsWithChildren) {
         />
       </head>
       <body className="overflow-y-scroll">
-        <header className="container mx-auto">
-          <nav aria-label="사이트 페이지 내비게이션">
-            <ul className="p-5">
+        <header className="bg-slate-900 text-white">
+          <nav
+            className="container mx-auto"
+            aria-label="사이트 페이지 내비게이션"
+          >
+            <ul className=" flex gap-x-4 p-5">
               <li>
-                <Link href="/">홈</Link>
+                <NavLink href="/">홈</NavLink>
               </li>
               <li>
-                <Link href="/auth/sign-in">로그인</Link>
+                <NavLink href="/auth/sign-in">로그인</NavLink>
               </li>
               <li>
-                <Link href="/books">도서 목록</Link>
+                <NavLink href="/auth/sign-up" activeClassName="text-sky-500">
+                  회원가입
+                </NavLink>
               </li>
               <li>
-                <Link href="/dashboard">대시보드</Link>
+                <NavLink href="/books">도서 목록</NavLink>
+              </li>
+              <li className="relative group">
+                <NavLink href="/dashboard" exact>
+                  {/* exact : 현재 보여지는 path가 페이지와 정확히 일치할때 보여준다~ */}
+                  대시보드
+                </NavLink>
+                <ul
+                  className={tw`
+                  group-hover:block
+                  group-focus-within:block
+                  hidden
+                  absolute -left-2 
+                  w-[12ch]
+                  space-y-2
+                  px-3 pt-2 pb-3 rounded
+                  bg-slate-950
+                  `}
+                >
+                  <li>
+                    <NavLink href="/dashboard/profile">프로필</NavLink>
+                  </li>
+                  <li>
+                    <NavLink href="/dashboard/settings">설정</NavLink>
+                  </li>
+                </ul>
               </li>
             </ul>
           </nav>
