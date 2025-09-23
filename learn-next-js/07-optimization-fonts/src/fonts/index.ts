2381b82f39b4fbe212a87d7eac70505cb3ef1163
wjinss <aastu4343@gmail.com>
2025-09-23 15:28:32 +0900

    learn. Next.js의 폰트 최적화 2 - Google 비가변 폰트 - Gothid A1 - 폰트 구성 모듈화 (성능, 최적화)

---
+++ learn-next-js/07-optimization-fonts/src/fonts/index.ts
@@ -0,0 +1,12 @@
+import { Noto_Sans_KR, Gothic_A1 } from 'next/font/google'
+
+export const notoSansKR = Noto_Sans_KR({
+  subsets: ['latin'],
+  variable: '--noto-sans-kr',
+})
+
+export const gothicA1 = Gothic_A1({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--gothic-a1',
+})
