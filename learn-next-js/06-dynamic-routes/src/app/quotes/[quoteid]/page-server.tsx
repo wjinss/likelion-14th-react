c2fb0a6e66b32f8c9c73edc74af819dae8cda02a
wjinss <aastu4343@gmail.com>
2025-09-23 10:39:02 +0900

    learn. 클라이언트 컴포넌트에서 라우트 매개변수 값 가져오기 - next/navigation 모듈에서 useParams 훅 - 'use client' 디렉티브(지시어) 파일 상단에 선언

--- learn-next-js/06-dynamic-routes/src/app/quotes/[quoteid]/page-server.tsx
+++ learn-next-js/06-dynamic-routes/src/app/quotes/[quoteid]/page-server.tsx
@@ -2,13 +2,19 @@ import { notFound } from 'next/navigation'
 import { Section } from '@/components'
 import { tw } from '@/utils'
 import { fetchQuoteById } from '../api'
-import ShowParamsButton from './show-params-button'
 
-export default async function QuoteSinglePage({ params }: Props) {
+export default async function QuoteSinglePage({
+  params,
+}: {
+  params: Promise<{ quoteId: string }>
+}) {
   const { quoteId } = await params
+
   const quoteResponse = await fetchQuoteById(quoteId)
 
-  if ('message' in quoteResponse) notFound()
+  if ('message' in quoteResponse) {
+    notFound()
+  }
 
   return (
     <Section title="인용 구절">
@@ -16,16 +22,11 @@ export default async function QuoteSinglePage({ params }: Props) {
         className={tw`
           flex flex-col gap-y-4 
           p-5 border-5 border-slate-200 text-slate-700
-          `}
+        `}
       >
         <p>{quoteResponse.quote}</p>
         <cite>{quoteResponse.author}</cite>
       </blockquote>
-      <ShowParamsButton />
     </Section>
   )
-}
-
-interface Props {
-  params: Promise<{ quoteId: string }>
 }
